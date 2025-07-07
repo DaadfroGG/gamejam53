bbf5110bb34853bf8de4ca2e7df758c7f0dccc67
DaadfroGG <d.kermia@yahoo.com>
2025-07-07 19:34:51 +0200

    merge mergem erge

--- cam_ctrl.gd
+++ cam_ctrl.gd
@@ -27,20 +27,8 @@ var end_fov : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-<<<<<<< HEAD
-	cadre = cam_pos.base
-	get_marker(cadre)
-	beg_setup()
-
-	# Immediately place camera at the base marker position
-	cam.global_position = end_pos
-	cam.global_rotation = end_rot
-	cam.fov = end_fov
-
-=======
 	go_base()
 	pass # Replace with function body.
->>>>>>> refs/remotes/origin/main
 
 
 func go_base():
