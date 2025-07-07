9ca4555f3cada71565b616c6334c6cd1338a6cde
= <adri12tenapro@gmail.com>
2025-07-07 23:40:08 +0200

    on est en exam machine ou quoi? (ajoute de la bulle au bonne endroit)

--- interact_gui.gd
+++ interact_gui.gd
@@ -2,15 +2,19 @@ extends Control
 
 @export var pnj : Node
 @export var player : Node
+@export var pnj_pos : Marker3D
+@export var cam : Camera3D
 @export var is_active : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	self.position = cam.unproject_position(pnj_pos.global_position)
 	if (not is_active):
 		self.visible = false
 	else:
