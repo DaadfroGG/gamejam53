25c8170defd8d4bc88cd99b9c148331a1af531bf
= <adri12tenapro@gmail.com>
2025-07-08 19:44:39 +0200

    le debut des scenario par ordinateur dans des monde virtuelle dans la societer pnjlabbeavior

---
+++ resources/action.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name Action
+
+@export var qui: String
+@export var moment: float
+@export var type: String
+@export var target: String
+
+@export var is_finish: bool
---
+++ story_line.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@export var story_actions : Array[Action]
+
+var already_triggered : Array[Action] = []
+
+func _process(delta: float) -> void:
+	var current_moment = AutoRun.moment
+
+	for action in story_actions:
+		if action.moment <= current_moment and action not in already_triggered:
+			print("Action déclenchée :", action.qui, action.type, action.target)
+			already_triggered.append(action)
